3ca52dc7e6b9b63acf5700e71abe033205a22c80
Vagner dos Santos Behs <vagner.behs@pampa.compasso>
2020-12-02 11:45:04 -0300

    Atualização página home

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
-import {useState} from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
         <div>
-            <h1>Contador</h1>
-            <Contador />
+            <h1>Página Home</h1>
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
         </div>
     )
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionaContador() {
-        setContador(contador + 1)
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionaContador}>Adicionar</button>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
