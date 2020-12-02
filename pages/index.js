5f9362f0edc4d7dc8ea3716294f0072604881661
Vagner dos Santos Behs <vagner.behs@pampa.compasso>
2020-12-02 11:25:35 -0300

    Link e página sobre criados

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,34 @@
-import {useState} from 'react';
+// import {useState} from 'react';
+
+// function Home() {
+//     return (
+//         <div>
+//             <h1>Página inicial</h1>
+//             <Contador />
+//         </div>
+//     )
+// }
+
+// function Contador() {
+//     const [contador, setContador] = useState(1);
+
+//     function adicionaContador() {
+//         setContador(contador + 1)
+//     }
+
+//     return (
+//         <div>
+//             <div>{contador}</div>
+//             <button onClick={adicionaContador}>Adicionar</button>
+//         </div>
+//     )
+// }
 
 function Home() {
     return (
         <div>
             <h1>Página inicial</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionaContador() {
-        setContador(contador + 1)
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionaContador}>Adicionar</button>
+            <a href="/sobre">Acessar página sobre</a>
         </div>
     )
 }
---
+++ pages/sobre.js
@@ -0,0 +1,10 @@
+function Sobre() {
+    return (
+        <div>
+            <h1>Página Sobre</h1>
+            <a href="/">Acessar página Home</a>
+        </div>
+    )
+}
+
+export default Sobre;
